80ffc5ceacd93de43519d40050043ddf54d995c6
Rafael <bechstedt@hotmail.com>
2023-11-09 17:26:49 -0300

    a

--- src/main/java/com/betrybe/weatherreport/WeatherClientImpl.java
+++ src/main/java/com/betrybe/weatherreport/WeatherClientImpl.java
@@ -7,13 +7,13 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 /**
  * Javadoc.
  */
-@RestController
+@Service
 public class WeatherClientImpl implements WeatherClient {
 
 
